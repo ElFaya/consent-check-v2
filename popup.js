79e04125d54a9d12ea167bc682c051d9ab2a2e50
ElFaya <159311362+ElFaya@users.noreply.github.com>
2024-02-27 21:35:49 +0100

    update

--- popup.js
+++ popup.js
@@ -147,53 +147,130 @@ function extractGcdValue(url) {
 }
 // Fonction principale pour mapper les noms de service et récupérer les valeurs de gcd pour chaque URL
 function mapUrlsAndGcdValues(urls) {
-  const mappedData = [];
-
+  const mappedData = {};
+  
   urls.forEach(url => {
     const serviceName = mapServiceName(url);
     const gcdValue = extractGcdValue(url);
-    const gcdStatus = getGcdStatus(gcdValue); // Obtention du statut GCD en fonction de sa valeur
-
-    mappedData.push({
-      url: url,
-      serviceName: serviceName,
-      gcdValue: gcdValue,
-      gcdStatus: gcdStatus // Ajout du statut GCD dans les données mappées
-    });
+    const gcdStatus = getGcdStatus(gcdValue);
+    let serviceId;
+    const catValue = extractCatValue(url);
+    const typeValue = extractTypeValue(url);
+    
+    if (serviceName === 'DV & CM' || serviceName === 'Piggyback') {
+      serviceId = extractSrcValue(url);
+    } else if (serviceName === 'Google Analytics') {
+      serviceId = extractGaId(url);
+    } else if (serviceName === 'Google Ads') {
+      serviceId = extractAwId(url);
+    }
+    
+    // Création d'une clé unique basée sur gcdValue, serviceId, catValue et typeValue
+    const key = `${gcdValue}_${serviceId}_${catValue}_${typeValue}`;
+    
+    // Vérification si la clé existe déjà dans mappedData
+    if (!mappedData[key]) {
+      mappedData[key] = {
+        url: url,
+        serviceName: serviceName,
+        gcdValue: gcdValue,
+        gcdStatus: gcdStatus,
+        serviceID: serviceId,
+        catValue: catValue,
+        typeValue: typeValue
+      };
+    }
   });
 
-  return mappedData;
+  // Transformation de l'objet en tableau avant de le retourner
+  return Object.values(mappedData);
 }
 
+// Ajoutez cette fonction à votre fichier popup.js
+function toggleExtraInfo(index) {
+  const extraInfoRow = document.getElementById(`extraInfo${index}`);
+  const button = document.getElementById(`toggleButton${index}`);
+  if (extraInfoRow.style.display === 'none') {
+    extraInfoRow.style.display = 'table-row';
+    button.innerHTML = "-";
+  } else {
+    extraInfoRow.style.display = 'none';
+    button.innerHTML = "+";
+  }
+}
 
-// Fonction pour créer le tableau HTML avec les données mappées
+// Modifiez la fonction createTable existante pour inclure le bouton "+" et appeler la fonction toggleExtraInfo
 function createTable(mappedData) {
   // Début de la construction du tableau HTML
-  let htmlTable = '<table id= resultTable border="1"><tr><th></th><th>Service</th><th>GCD</th><th>Statut</th><th></th></tr>';
+  let htmlTable = '<table id="resultTable" border="1"><tr><th></th><th></th><th>Service</th><th>GCD</th><th>Statut</th></tr>';
 
-   // Itération sur les données mappées
-  mappedData.forEach(entry => {
-	 if (entry.serviceName !== 'Unknown'){
-		const serviceIcon = getServiceIcon(entry.serviceName);
-		const gcdStatusIcon = getGcdStatusIcon(entry.gcdStatus); // Utilisation correcte comme un objet
-		const gcdStatus = getGcdStatus(entry.gcdValue);
+  // Itération sur les données mappées
+  mappedData.forEach((entry, index) => {
+    if (entry.serviceName !== 'Unknown') {
+      const serviceIcon = getServiceIcon(entry.serviceName);
+      const gcdStatusIcon = getGcdStatusIcon(entry.gcdStatus); // Utilisation correcte comme un objet
+      const gcdStatus = getGcdStatus(entry.gcdValue);
+      let extraInfo = '';
 
-		// Construction de chaque ligne du tableau avec les données de l'entrée
-		htmlTable += `<tr>
-						<td><img src="${serviceIcon}" alt="Service Icon" class="icons"></td>
-						<td>${entry.serviceName}</td>
-						<td>${entry.gcdValue}</td>
-						<td>${gcdStatus}</td>
-						<td><img src="${gcdStatusIcon}" alt="GCD Status Icon" class="icons"></td>
-					 </tr>`;
-  }});
+      // Construction de chaque ligne du tableau avec les données de l'entrée
+      htmlTable += `<tr>
+                      <td><button id="toggleButton${index}">+</button></td>
+                      <td><img src="${serviceIcon}" alt="Service Icon" class="icons"></td>
+                      <td>${entry.serviceName}</td>
+                      <td>${entry.gcdValue}</td>
+                      <td>${gcdStatus}</td>
+                      <td><img src="${gcdStatusIcon}" alt="GCD Status Icon" class="resultIcon"></td>
+                   </tr>`;
+
+      // Ajouter la ligne supplémentaire
+      htmlTable += `<tr id="extraInfo${index}" class="results" style="display: none;">
+                      <td colspan="5">
+                        <img src="images/arrow.png" alt="Arrow Icon" class="icons">`;
+
+      // Vérifier si le nom du service est 'DV & CM' ou 'Piggyback'
+      if (entry.serviceName === 'DV & CM' || entry.serviceName === 'Piggyback') {
+        extraInfo += `<strong>Src=</strong>${entry.serviceID}`;
+      } else {
+        extraInfo += `<strong>Account ID=</strong>${entry.serviceID}`;
+      }
+      
+      // Vérifier si Cat est différent de null avant de l'ajouter
+      if (extractCatValue(entry.url) !== null) {
+        extraInfo += ` | <strong>Cat=</strong>${extractCatValue(entry.url)}`;
+      }
+      
+      // Vérifier si Type est différent de null avant de l'ajouter
+      if (extractTypeValue(entry.url) !== null) {
+        extraInfo += ` | <strong>Type=</strong>${extractTypeValue(entry.url)}`;
+      }
+      
+      // Ajout des informations supplémentaires
+      htmlTable += extraInfo;
+
+      // Ajout de l'icône URL avec une infobulle contenant l'URL
+      htmlTable += ` | <img src="images/url-icon.png" class="icons" title="${entry.url}">`;
+
+      htmlTable += `</td></tr>`;
+    }
+  });
 
   // Fermeture du tableau HTML
   htmlTable += '</table>';
 
   // Écrire le tableau HTML dans le document
-  document.getElementById("resultsContainer").innerHTML = htmlTable + '</table>';
+  document.getElementById("resultsContainer").innerHTML = htmlTable;
+
+  // Ajouter des écouteurs d'événements aux boutons
+  mappedData.forEach((entry, index) => {
+    if (entry.serviceName !== 'Unknown') {
+      const button = document.getElementById(`toggleButton${index}`);
+      button.addEventListener('click', () => {
+        toggleExtraInfo(index);
+      });
+    }
+  });
 }
+
 
 // Exemple d'utilisation avec les données mappées
 const mappedData = [];
@@ -225,19 +302,30 @@ document.getElementById('premium').addEventListener('click', function() {
 /////////////////////test //
 
 function extractSrcValue(url) {
-  // Sépare les paramètres de l'URL en un tableau
-  const paramsArray = url.split(/[?&;]/);
-  
-  // Parcourt chaque paramètre pour trouver celui qui commence par "src=" ou "src/"
-  for (const param of paramsArray) {
-    if (param.startsWith('src=') || param.startsWith('src/')) {
-      // Récupère la valeur du paramètre
-      return param.substring(param.indexOf('=') + 1);
+    // Diviser l'URL en utilisant "src=" comme séparateur
+    const parts = url.split('src=');
+    
+    // Si la division a généré plus d'une partie, la valeur de src est la deuxième partie
+    if (parts.length > 1) {
+        // Si la deuxième partie contient un autre séparateur, on le divise à nouveau
+        const srcValue = parts[1].split(/[&;]/)[0];
+        // Retourne la valeur de src, après avoir enlevé d'éventuels caractères spéciaux
+        return decodeURIComponent(srcValue);
     }
-  }
-  
-  // Retourne null si aucun paramètre "src" n'est trouvé
-  return null;
+    
+    // Si "src=" n'a pas été trouvé, essayons avec "src/"
+    const partsWithSlash = url.split('src/');
+    
+    // Si la division avec "src/" a généré plus d'une partie, la valeur de src est la deuxième partie
+    if (partsWithSlash.length > 1) {
+        // Si la deuxième partie contient un autre séparateur, on le divise à nouveau
+        const srcValue = partsWithSlash[1].split(/[&;]/)[0];
+        // Retourne la valeur de src, après avoir enlevé d'éventuels caractères spéciaux
+        return decodeURIComponent(srcValue);
+    }
+    
+    // Si ni "src=" ni "src/" n'ont été trouvés, retourne null
+    return null;
 }
 
 function extractCatValue(url) {
@@ -253,6 +341,43 @@ function extractTypeValue(url) {
   const params = new URLSearchParams(url);
   return params.get('type');
 }
+
+
+function extractGaId(url) {
+    // Recherche de la partie de l'URL qui contient le paramètre 'tid'
+    const tidIndex = url.indexOf('tid=');
+    if (tidIndex === -1) {
+        // Si le paramètre 'tid' n'est pas trouvé, renvoyer une chaîne vide
+        return '';
+    }
+
+    // Extraire la sous-chaîne à partir de 'tid=' jusqu'au prochain caractère '&' ou la fin de la chaîne
+    const tidSubstring = url.substring(tidIndex + 4);
+    const nextAmpersandIndex = tidSubstring.indexOf('&');
+    const tid = nextAmpersandIndex !== -1 ? tidSubstring.substring(0, nextAmpersandIndex) : tidSubstring;
+
+    return tid;
+}
+
+
+function extractAwId(url) {
+    // Diviser l'URL en parties en utilisant le caractère "/"
+    const parts = url.split("/");
+    let awId = '';
+
+    // Parcourir les parties pour trouver l'identifiant AW
+    for (let i = 0; i < parts.length; i++) {
+        // Si la partie actuelle commence par des chiffres et se compose uniquement de chiffres, c'est probablement l'identifiant AW
+        if (/^\d+$/.test(parts[i])) {
+            awId = parts[i];
+            break; // Sortir de la boucle dès que l'identifiant est trouvé
+        }
+    }
+
+    // Ajouter la chaîne "AW-" devant l'identifiant extrait
+    return awId !== '' ? "AW-" + awId : '';
+}
+
 
 function mapFloodlightValue(urls) {
   const mappedFloodlight = [];
